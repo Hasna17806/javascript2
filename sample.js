6a37287ea4686976789e0e37befaa39d9482b921
Hasna17806 <hasnahamza807@gmail.com>
2025-07-30 20:43:19 -0700

    30 july

---
+++ sample.js
@@ -0,0 +1,10 @@
+document.write("Learn javascript");
+alert("Hello World");
+document.write("<h1>Good Morning</h1")
+/*this is a comment*/
+//Variables & Data Types
+
+
+
+var a = 10, b =20;
+document.write(a + b)
